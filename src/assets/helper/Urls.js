cd95e706a6d28d08fd3f5aca65c5146442835929
mughira0 <mughiraubaid432@gmail.com>
2025-05-29 17:21:11 +0500

    chamhes

--- src/assets/helper/Urls.js
+++ src/assets/helper/Urls.js
@@ -1,5 +1,5 @@
-// const apiUrl = "https://blogs-db-backend.vercel.app";
-const apiUrl = "http://localhost:5000";
+const apiUrl = "https://blogs-db-backend.vercel.app";
+// const apiUrl = "http://localhost:5000";
 export const BaseURL = (api) => {
   const url = `${apiUrl}/api/v1/${api}`;
   return url;
